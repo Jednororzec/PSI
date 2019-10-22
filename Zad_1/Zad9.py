0d42dcaafa847a34db5af8be427df9531b02fffe
Bartlomiej <bartekborkowski13@gmail.com>
2019-10-22 21:22:53 +0200

    Zadanie9

---
+++ Zad_1/Zad9.py
@@ -0,0 +1,6 @@
+letterDict = {'Adam Nowak':['22','adam@wp.pl','02.09.1997','Warszawa'],\
+              'Adam Kowalski':['23','adddaam@wp.pl','06.12.1997','Olsztyn'],\
+              'Robert Kwiatkowski':['22','robb11@gmail.com','11.12.1995','Warszawa'],\
+              'Iza Nowak':['21','iska@wp.pl','18.12.1998','Gdansk']}
+print(letterDict['Adam Nowak'])
+print(letterDict['Iza Nowak'])
